50530e42ca123baebcf7737c2333e6e39b35b149
Ashwani Singh <singhashwanikumar793@gmail.com>
2024-05-09 15:21:44 +0530

    checking multer file lcoation

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,15 +1,20 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
-  },
-  filename: function (req, file, cb) {
-    console.log("file from multer", file);
-    cb(null, file.originalname);
-  },
+	destination: function (req, file, cb) {
+		// Specify the absolute path to the "public/temp" directory
+		const uploadPath = path.join(__dirname, "../../public/temp");
+		// cb(null, "./public/temp");
+		cb(null, uploadPath);
+
+		// cb(null, "./p");
+	},
+	filename: function (req, file, cb) {
+		console.log("file from multer", file);
+		cb(null, file.originalname);
+	},
 });
 
 export const upload = multer({
-  storage,
+	storage,
 });
