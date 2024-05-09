82b732f68fe37d1c990fe0f5fc81bfc7ea2992f7
Ashwani Singh <singhashwanikumar793@gmail.com>
2024-05-09 16:32:54 +0530

    checking in prod

--- src/utils/cloudinaryfile.js
+++ src/utils/cloudinaryfile.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: process.env.CLODUINARY_CLOUD_NAME,
-  api_key: process.env.CLODUINARY_API_KEY,
-  api_secret: process.env.CLODUINARY_API_SECRET,
+	cloud_name: process.env.CLODUINARY_CLOUD_NAME,
+	api_key: process.env.CLODUINARY_API_KEY,
+	api_secret: process.env.CLODUINARY_API_SECRET,
 });
 
 // Return "https" URLs by setting secure: true
@@ -39,26 +39,26 @@ cloudinary.config({
 // export { uploadOnCloudinary };
 
 const uploadOnCloudinary = async (localFilePath) => {
-  console.log("local path ", localFilePath);
+	console.log("local path ", localFilePath);
 
-  try {
-    if (!localFilePath) {
-      console.log("local file path not there");
-      // return null;
-    }
-    //upload the file on cloudinary
-    const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
-    });
-    // file has been uploaded successfull
-    console.log("file is uploaded on cloudinary ", response);
-    fs.unlinkSync(localFilePath);
-    return response;
-  } catch (error) {
-    fs.unlinkSync(localFilePath);
-    // remove the locally saved temporary file as the upload operation got failed
-    return null;
-  }
+	try {
+		if (!localFilePath) {
+			console.log("local file path not there");
+			// return null;
+		}
+		//upload the file on cloudinary
+		const response = await cloudinary.uploader.upload(localFilePath, {
+			resource_type: "auto",
+		});
+		// file has been uploaded successfull
+		console.log("file is uploaded on cloudinary ", response);
+		fs.unlinkSync(localFilePath);
+		return response;
+	} catch (error) {
+		fs.unlinkSync(localFilePath);
+		// remove the locally saved temporary file as the upload operation got failed
+		return null;
+	}
 };
 
 export { uploadOnCloudinary };
