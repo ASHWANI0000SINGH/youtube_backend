3db1911c2d229da3d1a5b249669e32bce4edcb9e
Ashwani Singh <singhashwanikumar793@gmail.com>
2024-05-11 11:33:20 +0530

    fetch video all route updated removed jwt

--- src/routes/videoUpload.route.js
+++ src/routes/videoUpload.route.js
@@ -9,10 +9,12 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
-router.use(verifyJWT);
-router.route("/upload").post(upload.single("videoFile"), uploadVideo);
-router.route("/fetch-user-video/:id").get(fetchUserVideos);
+// router.use(verifyJWT);
+router
+	.route("/upload")
+	.post(upload.single("videoFile"), verifyJWT, uploadVideo);
+router.route("/fetch-user-video/:id").get(verifyJWT, fetchUserVideos);
 router.route("/fetch-allvideo").get(fetchAllVideos);
-router.route("/fetch-byId/:id").get(fetchVideoById);
+router.route("/fetch-byId/:id").get(verifyJWT, fetchVideoById);
 
 export default router;
